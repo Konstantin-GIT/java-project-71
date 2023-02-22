630d5349406bcbfc614e724af172a7687b91e184
Konstantin Naumovich <konstantin000008@gmail.com>
2023-02-22 14:27:28 +0300

    added option '-f' output format

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,23 @@
 package hexlet.code;
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
 @Command(name = "gendiff",  version = "Differ 1.0", mixinStandardHelpOptions = true,  description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable{
-   // @Override
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]", defaultValue = "stylish")
+    String format;
+
+    @Parameters(paramLabel = "filepath1",
+            description = "path to first file")
+    private String filepath1;
+
+    @Parameters(paramLabel = "filepath2",
+            description = "path to second file")
+    private String filepath2;
+    // @Override
     public void run() {
         // The business logic of the command goes here...
         // In this case, code for generation of ASCII art graphics
