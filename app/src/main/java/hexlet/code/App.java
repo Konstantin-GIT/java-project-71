0b5f5b97dc4d2f5f324653e8243c4a982ae8f09f
Konstantin Naumovich <konstantin000008@gmail.com>
2023-02-22 13:23:50 +0300

    added Picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
-public class App {
+@Command(name = "gendiff",  version = "Differ 1.0", mixinStandardHelpOptions = true,  description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable{
+   // @Override
+    public void run() {
+        // The business logic of the command goes here...
+        // In this case, code for generation of ASCII art graphics
+        // (omitted for the sake of brevity).
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
+
