07457dc901fafa6b0f7ad54a8074b376ad59789d
Konstantin Naumovich <konstantin000008@gmail.com>
2023-02-23 14:42:47 +0300

    added Differ class and generate method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,29 +4,40 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 
-@Command(name = "gendiff",  version = "Differ 1.0", mixinStandardHelpOptions = true,  description = "Compares two configuration files and shows a difference.")
-public class App implements Runnable{
+import java.io.IOException;
+import java.util.concurrent.Callable;
 
+@Command(name = "gendiff",  version = "Differ 1.0", mixinStandardHelpOptions = true,  description = "Compares two configuration files and shows a difference.")
+public class App implements Callable {
     @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]", defaultValue = "stylish")
     String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filePath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filePath2;
 
-    @Parameters(paramLabel = "filepath1",
-            description = "path to first file")
-    private String filepath1;
 
-    @Parameters(paramLabel = "filepath2",
-            description = "path to second file")
-    private String filepath2;
-    // @Override
-    public void run() {
-        // The business logic of the command goes here...
-        // In this case, code for generation of ASCII art graphics
-        // (omitted for the sake of brevity).
+    @Override
+    public String call() throws IOException {
+        String differBetweenTwoFiles = "Method execution error";
+        try {
+            differBetweenTwoFiles = Differ.generate(filePath1, filePath2);
+        } catch ( IOException e) {
+            System.out.println(e);
+            return e.toString();
+        }
+        return differBetweenTwoFiles;
     }
 
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
+    public static void main(String[] args) throws IOException {
+        try {
+            App app = new App();
+            int exitCode = new CommandLine(app).execute(args);
+            System.exit(exitCode);
+            app.call();
+        } catch (IOException e) {
+            System.out.println();
+        }
     }
 }
 
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,42 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Differ {
+
+    public static String generate(String filePath1, String filePath2) throws IOException {
+        Path file1 = Paths.get(filePath1);
+        Path file2 = Paths.get(filePath2);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> jsonToParse1 = objectMapper.readValue(file1.toFile(), new TypeReference<Map<String,Object>>(){});
+        Map<String, Object> jsonToParse2 = objectMapper.readValue(file2.toFile(), new TypeReference<Map<String,Object>>(){});
+        Set<String> keys = new TreeSet<>(jsonToParse1.keySet());
+        keys.addAll(jsonToParse2.keySet());
+        StringBuilder differJso1AndJson2 = new StringBuilder();
+        differJso1AndJson2.append("{\n");
+        for (String key: keys) {
+            if (!jsonToParse1.containsKey(key)) {
+                differJso1AndJson2.append("  + " +  key + ": " + jsonToParse2.get(key) + "\n");
+            } else if (!jsonToParse2.containsKey(key)) {
+                differJso1AndJson2.append("  - " +  key + ": " + jsonToParse1.get(key) + "\n");
+            } else if (jsonToParse1.get(key).equals(jsonToParse2.get(key))) {
+                differJso1AndJson2.append("    " +  key + ": " + jsonToParse1.get(key) + "\n");
+            } else {
+                differJso1AndJson2.append("  - " +  key + ": " + jsonToParse1.get(key) + "\n");
+                differJso1AndJson2.append("  + " +  key + ": " + jsonToParse2.get(key) + "\n");
+            }
+        }
+        differJso1AndJson2.append("}");
+        System.out.println(differJso1AndJson2.toString());
+        return differJso1AndJson2.toString();
+    }
+}
