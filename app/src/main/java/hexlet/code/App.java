cf5ca49aa3ee6763e80b0bebf7cfcab8d9209dd6
Konstantin Naumovich <konstantin000008@gmail.com>
2023-03-09 15:24:31 +0300

    changed App class to final

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,10 +9,10 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",  version = "gendiff 1.0", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable {
+public final class App implements Callable {
     @Option(names = { "-f", "--format" }, paramLabel = "format",
         description = "output format [default: stylish]", defaultValue = "stylish")
-    public static String format;
+    private static String format;
     @Parameters(paramLabel = "filepath1", description = "path to first file")
     private String filePath1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
