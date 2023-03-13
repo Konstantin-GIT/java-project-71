0bf8c6e5508953a9c7fb0d375a1e22bd11d52c08
Konstantin Naumovich <konstantin000008@gmail.com>
2023-03-13 16:00:59 +0300

    changed Parser class

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,6 +1,12 @@
 package hexlet.code;
 
+import org.apache.commons.io.FilenameUtils;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.List;
 import static hexlet.code.Differences.getDifferences;
@@ -9,10 +15,16 @@ import static hexlet.code.Parser.getData;
 public class Differ {
 
     public static String generate(String filePath1, String filePath2, String format) throws IOException {
-        Map<String, Object> dataOfFile1 = getData(filePath1);
-        Map<String, Object> dataOfFile2 = getData(filePath2);
+        String fileExtension1 = getExtensionFile(filePath1);
+        String fileExtension2 = getExtensionFile(filePath2);
 
-        List<DataDifferences>  dataDifferences = getDifferences(dataOfFile1, dataOfFile2);
+        String dataByFile1 = readFile(filePath1, StandardCharsets.UTF_8);
+        String dataByFile2 = readFile(filePath2, StandardCharsets.UTF_8);
+
+        Map<String, Object> data1 = getData(dataByFile1, fileExtension1);
+        Map<String, Object> data2 = getData(dataByFile2, fileExtension2);
+
+        List<DataDifferences>  dataDifferences = getDifferences(data1, data2);
 
         return Formatter.outputDifferences(dataDifferences, format);
     }
@@ -20,4 +32,15 @@ public class Differ {
     public static String generate(String filePath1, String filePath2) throws IOException {
         return generate(filePath1, filePath2, "stylish");
     }
+
+    private static String readFile(String path, Charset encoding) throws IOException {
+        Path fullPath = Paths.get(path).toAbsolutePath().normalize();
+        return Files.readString(fullPath, encoding);
+    }
+
+
+    public static String getExtensionFile(String filename) {
+        return FilenameUtils.getExtension(filename);
+    }
+
 }
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,26 +3,21 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-import org.apache.commons.io.FilenameUtils;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Parser {
 
-    public static Map<String, Object> getData(String filePath) throws IOException {
-        ObjectMapper mapperForFile = getDataMapper(filePath);
-        File file = getFileByPath(filePath);
-        Map<String, Object> result = mapperForFile.readValue(file, new TypeReference<Map<String, Object>>() { });
+    public static Map<String, Object> getData(String data, String formatData) throws IOException {
+        ObjectMapper dataMapper = getDataMapper(formatData);
+        Map<String, Object> result = dataMapper.readValue(data, new TypeReference<Map<String, Object>>() { });
         return result;
     }
 
-    public static ObjectMapper getDataMapper(String filePath) {
-        if (getExtensionFile(filePath).equals("json")) {
+    public static ObjectMapper getDataMapper(String formatData) {
+        if (formatData.equals("json")) {
             return new ObjectMapper();
-        } else if (getExtensionFile(filePath).equals("yml")) {
+        } else if (formatData.equals("yml")) {
             return new YAMLMapper();
         } else {
             System.out.println("Error: invalid file format.");
@@ -30,14 +25,7 @@ public class Parser {
         return new ObjectMapper();
     }
 
-    public static File getFileByPath(String filePath) {
-        Path fullPath = Paths.get(filePath).toAbsolutePath().normalize();
-        return fullPath.toFile();
-    }
 
-    public static String getExtensionFile(String filename) {
-        return FilenameUtils.getExtension(filename);
-    }
 
 
 
